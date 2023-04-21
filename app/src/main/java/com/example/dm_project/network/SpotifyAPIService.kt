d9ffe976c143cfd45551815a7c31a687bcd04eb6
DeeDeeShiba <a.davidromero@icloud.com>
2023-04-21 14:38:52 -0400

    added overview package

--- app/src/main/java/com/example/dm_project/network/SpotifyAPIService.kt
+++ app/src/main/java/com/example/dm_project/network/SpotifyAPIService.kt
@@ -36,8 +36,8 @@ interface SpotifyApiService { //Fetch artist spoitfy ID given string name
         "X-RapidAPI-Key: 8388516373msh1227ac17fbff7f3p1069c8jsna28adae39e46",
         "X-RapidAPI-Host: spotify23.p.rapidapi.com"
     )
-    @GET("")
-    fun getArtistID(): List<SpotifyID> //TODO: Add later
+    @GET("artist_overview")
+    suspend fun getArtistOverview()
 }
 
 object SpotifyAPI {
