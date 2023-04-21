c5f1c018f4dd230ee1f3fdf0b8f29e8833147cb0
DeeDeeShiba <a.davidromero@icloud.com>
2023-04-21 17:06:23 -0400

    updated Dataclass

--- app/src/main/java/com/example/dm_project/network/ArtistOverview.kt
+++ app/src/main/java/com/example/dm_project/network/ArtistOverview.kt
@@ -1,5 +1,7 @@
 package com.example.dm_project.network
 
+import android.media.Image
+import com.beust.klaxon.Klaxon
 import com.squareup.moshi.Json
 
 /**
@@ -9,10 +11,46 @@ import com.squareup.moshi.Json
  * on 2023-04-21
  **/
 
-//Class used to handle Spotify Artist information
-data class ArtistOverview(
-    val id: String,
-    // used to map img_src from the JSON to imgSrcUrl in our class
-    //@Json(name = "img_src") val imgSrcUrl: String
 
+
+
+
+private val klaxon = Klaxon()
+
+
+data class Welcome4 (
+    val artists: List<Artist>
+) {
+    public fun toJson() = klaxon.toJsonString(this)
+
+    companion object {
+        public fun fromJson(json: String) = klaxon.parse<Welcome4>(json)
+    }
+}
+data class Artist (
+    @Json(name = "external_urls")
+    val externalUrls: ExternalUrls,
+
+    val followers: Followers,
+    val genres: List<String>,
+    val id: String,
+    val images: List<Image>,
+    val name: String,
+    val popularity: Long,
+    val type: String,
+    val uri: String
+)
+data class ExternalUrls (
+    val spotify: String
+)
+
+data class Followers (
+    val href: Any? = null,
+    val total: Long
+)
+
+data class Image (
+    val height: Long,
+    val url: String,
+    val width: Long
 )
